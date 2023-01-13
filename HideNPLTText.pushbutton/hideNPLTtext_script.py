3f476be1439b8040a707f2ad3506bc9d764ef094
Dan Howard <dhoward@Ballinger.com>
2023-01-13 16:15:58 -0500

    add hide/unhide for dims and details lines

--- HideNPLTText.pushbutton/hideNPLTtext_script.py
+++ HideNPLTText.pushbutton/hideNPLTtext_script.py
@@ -1,5 +1,5 @@
-__title__ = "Hide\n NPLT Text"
-__doc__ = "Hide all NPLT Text in the active project"
+__title__ = "Hide\n NPLT Elements"
+__doc__ = "Hide all NPLT Text, Detail Lines, and Dimensions in the active project"
 __author__ = "D. Howard, Ballinger"
 
 from Autodesk.Revit.DB import *
@@ -10,6 +10,8 @@ from pyrevit import forms
 doc = __revit__.ActiveUIDocument.Document
 
 NPLT_notes = [n for n in FilteredElementCollector(doc).OfClass(TextNote) if "NPLT" in n.Name]
+NPLT_lines = [e for e in FilteredElementCollector(doc).OfClass(CurveElement) if type(e) == DetailLine and "NPLT" in e.LineStyle.Name]
+NPLT_dims = [d for d in FilteredElementCollector(doc).OfClass(Dimension) if d.Category.Name == "Dimensions" and "NPLT" in d.Name]
 
 views = {}
 
@@ -19,22 +21,39 @@ for n in NPLT_notes:
     else:
         views[n.OwnerViewId.IntegerValue].Add(n.Id)
 
+for elem in NPLT_lines:
+    if elem.OwnerViewId.IntegerValue not in views:
+        views[elem.OwnerViewId.IntegerValue] = List[ElementId]([elem.Id])
+    else:
+        views[elem.OwnerViewId.IntegerValue].Add(elem.Id)
+
+for d in NPLT_dims:
+    if d.OwnerViewId.IntegerValue not in views:
+        views[d.OwnerViewId.IntegerValue] = List[ElementId]([d.Id])
+    else:
+        views[d.OwnerViewId.IntegerValue].Add(d.Id)
+
 num_notes = len(NPLT_notes)
+num_lines = len(NPLT_lines)
+num_dims = len(NPLT_dims)
 num_views = len(views)
 
-transaction_description = "{n} NPLT notes hidden in {v} views".format(n = num_notes, v = num_views)
+description =   "NPLT elements hidden in {v} views:\n" \
+                            "    Notes: {num_notes}\n" \
+                            "    Lines: {num_lines}\n" \
+                            "    Dims:  {num_dims}".format(num_notes = num_notes, v = num_views, num_lines=num_lines, num_dims=num_dims)
 
-t = Transaction(doc, transaction_description)
+t = Transaction(doc, "Hide NPLT Elements")
 t.Start()
 
-for view_id, note_list in views.items():
+for view_id, elem_list in views.items():
     view = doc.GetElement(ElementId(view_id))
-    view.HideElements(note_list)
+    view.HideElements(elem_list)
 
 t.Commit()
 
-forms.alert(transaction_description, 
-    sub_msg="This may include notes already hidden. Use `Unhide NPLT Text` to show hidden elements", 
-    title="Hide NPLT Notes",
+forms.alert(description, 
+    sub_msg="This may include elements already hidden. Use `Unhide NPLT` to show hidden elements", 
+    title="Hide NPLT Elements",
     ok=True,
     footer='Ballinger pyRevit Tools')
