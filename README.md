5387d5ac59f05f480d373cfe153c27c26239ad92
Howard <dhoward@Ballinger.com>
2023-12-07 11:49:23 -0500

    add tags to supported element types

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Hide/Unhide NPLT Text
-A pyRevit tool for hiding and unhiding in view all text notes containing "NPLT"
+A pyRevit tool for hiding and unhiding in view all annotations containing "NPLT"
 
-January 2023
+Works for:
+- Text Notes
+- Detail Lines
+- Dimensions
+- Tags
+
+v 1.0 January 2023
+v 1.1 November 30 2023 - update to hide/unhide in dependent views
+v 1.2 December 07 2023 - add tags to supported element types.
--- UnhideNPLT.pushbutton/unhideNPLT_script.py
+++ UnhideNPLT.pushbutton/unhideNPLT_script.py
@@ -12,6 +12,7 @@ doc = __revit__.ActiveUIDocument.Document
 NPLT_notes = [n for n in FilteredElementCollector(doc).OfClass(TextNote) if "NPLT" in n.Name and n.ViewSpecific]
 NPLT_lines = [e for e in FilteredElementCollector(doc).OfClass(CurveElement) if type(e) == DetailLine and "NPLT" in e.LineStyle.Name and e.ViewSpecific]
 NPLT_dims = [d for d in FilteredElementCollector(doc).OfClass(Dimension) if d.Category.Name == "Dimensions" and "NPLT" in d.Name and d.ViewSpecific]
+NPLT_tags = [t for t in FilteredElementCollector(doc).OfClass(IndependentTag) if "NPLT" in t.Name and t.ViewSpecific]
 
 views = {}
 
@@ -33,15 +34,23 @@ for d in NPLT_dims:
     else:
         views[d.OwnerViewId.IntegerValue].Add(d.Id)
 
+for t in NPLT_tags:
+    if t.OwnerViewId.IntegerValue not in views:
+        views[t.OwnerViewId.IntegerValue] = List[ElementId]([t.Id])
+    else:
+        views[t.OwnerViewId.IntegerValue].Add(t.Id)
+
 num_notes = len(NPLT_notes)
 num_lines = len(NPLT_lines)
 num_dims = len(NPLT_dims)
+num_tags = len(NPLT_tags)
 num_views = len(views)
 
 description =   "NPLT elements in {v} views:\n" \
                             "    Notes: {num_notes}\n" \
                             "    Lines: {num_lines}\n" \
-                            "    Dims:  {num_dims}".format(num_notes = num_notes, v = num_views, num_lines=num_lines, num_dims=num_dims)
+                            "    Dims:  {num_dims}\n" \
+                            "    Tags:  {num_tags}".format(num_notes = num_notes, v = num_views, num_lines=num_lines, num_dims=num_dims, num_tags=num_tags)
 
 owned_by_others = []
 
@@ -53,6 +62,18 @@ if num_views > 0:
         if WorksharingUtils.GetCheckoutStatus(doc, ElementId(view_id)) != CheckoutStatus.OwnedByOtherUser:
             view = doc.GetElement(ElementId(view_id))
             view.UnhideElements(elem_list)
+            if len(view.GetDependentViewIds()) > 0:
+                for dependent_id in view.GetDependentViewIds():
+                    if WorksharingUtils.GetCheckoutStatus(doc, dependent_id) != CheckoutStatus.OwnedByOtherUser:
+                        d_view = doc.GetElement(dependent_id)
+                        d_view.UnhideElements(elem_list)
+                    else:
+                        tip = WorksharingUtils.GetWorksharingTooltipInfo(doc, dependent_id)
+                        owner = tip.Owner
+                        viewname = doc.GetElement(dependent_id).Name
+                        num_items = len(elem_list)
+                        info = "{viewname}\n|  {num_items} elements \n|  owner: {owner}\n".format(viewname=viewname, num_items=num_items, owner=owner)
+                        owned_by_others.append(info)
         else:
             tip = WorksharingUtils.GetWorksharingTooltipInfo(doc, ElementId(view_id))
             owner = tip.Owner
